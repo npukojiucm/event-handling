cc9e2abec69c4bad99b3cc65ef97658ad8afea55
Aleksandr Platcev <a.platcev@ya.ru>
2023-04-04 22:51:43 +0300

    add solution

--- src/app.js
+++ src/app.js
@@ -1,54 +1,7 @@
-/* eslint-disable func-names */
-import goblin from './img/goblin.png';
+import GameController from './js/GameController';
+import GamePlay from './js/GamePlay';
 
-function getRandomInt() {
-  return Math.floor(Math.random() * 16);
-}
+const gamePlay = new GamePlay();
+const gameCtrl = new GameController(gamePlay);
 
-function createElement() {
-  const img = document.createElement('img');
-  img.className = 'img';
-  img.src = goblin;
-
-  return img;
-}
-
-function changeSection() {
-  const currentPosition = window.localStorage.getItem('position');
-  let nextPosition = getRandomInt();
-
-  while (nextPosition === currentPosition) {
-    nextPosition = getRandomInt();
-  }
-
-  const sections = document.querySelectorAll('.section');
-  const img = createElement();
-
-  sections[currentPosition].firstChild.remove();
-  sections[nextPosition].append(img);
-
-  window.localStorage.setItem('position', nextPosition);
-}
-
-function onClickImg() {
-  const container = document.querySelector('.container');
-
-  container.addEventListener('click', (event) => {
-    console.log(event);
-  });
-}
-
-function start() {
-  const startPosition = getRandomInt();
-  const img = createElement();
-  const sections = document.querySelectorAll('.section');
-
-  window.localStorage.setItem('position', startPosition);
-
-  onClickImg();
-
-  return sections[startPosition].append(img);
-}
-
-start();
-setInterval(changeSection, 1500);
+gameCtrl.init();
